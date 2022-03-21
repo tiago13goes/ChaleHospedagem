34b759a8e246ecbcfe666d9502eed9b0558914cb
Tiago Augusto Santos Góes <tiago.goes@pro4tech.com.br>
2022-03-21 14:58:47 -0300

    Tests

---
+++ ChaleHospedagem.Test/Services/TestServiceChale.cs
@@ -0,0 +1,37 @@
+﻿using ChaleHospedagem.Domain.Interface.Repositories;
+using ChaleHospedagem.Domain.Interface.Services;
+using ChaleHospedagem.Domain.Models;
+using ChaleHospedagem.Domain.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace ChaleHospedagem.Test.Services
+{
+    public class TestServiceChale
+    {
+        [Fact]
+        public void Post_Chale()
+        {
+            Chale chale = new()
+            {
+                codChale = 0,
+                capacidade = 9999999,
+                localizacao = "Post_Chale_Test",
+                valorAltaEstacao = 9999999,
+                valorBaixaEstacao = 999999,
+            };
+
+            IServiceChale _serviceChale = new Mock<IServiceChale>().Object;
+            //ServiceChale _serviceChale = new ServiceChale(new Mock<IRepositoryChale>().Object);
+
+            bool result = _serviceChale.Add(chale);
+            Assert.True(result);
+        }
+    }
+}
---
+++ ChaleHospedagem.Test/Services/TestServiceCliente.cs
@@ -0,0 +1,41 @@
+﻿using ChaleHospedagem.Domain.Interface.Repositories;
+using ChaleHospedagem.Domain.Interface.Services;
+using ChaleHospedagem.Domain.Models;
+using ChaleHospedagem.Domain.Services;
+using ChaleHospedagem.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ChaleHospedagem.Test.Services
+{
+    public class TestServiceCliente
+    {
+     
+        [Fact]
+        public void Post_Cliente()
+        {
+            var _serviceCliente = new ServiceCliente(new Mock<IRepositoryCliente>().Object);
+
+            Cliente cliente = new()
+            {
+                codCliente = 0,
+                bairroCliente = "Butantã",
+                CEPCliente = "12345-678",
+                cidadeCliente = "São Paulo",
+                enderecoCliente = "rua mmdc",
+                estadoCliente = "SP",
+                nascimentoCliente = DateTime.Now,
+                rgCliente = "12.345.678-9"
+            };
+
+            var result = _serviceCliente.Add(cliente);
+            Assert.True(result);
+        }
+    }
+}
